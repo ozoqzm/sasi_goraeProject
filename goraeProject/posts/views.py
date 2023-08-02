80b38112a0b9cb7540f59f2905acc925c1241188
chaechaeyeon <00dreamadream@kakao.com>
2023-08-02 19:23:02 +0900

    받은 메세지 목록, 메세지 작성

--- goraeProject/posts/views.py
+++ goraeProject/posts/views.py
@@ -1,7 +1,8 @@
 from rest_framework.response import Response
 from .models import Message
 from .serializers import MessageSerializer
-
+from django.shortcuts import get_object_or_404
+from django.contrib.auth.models import User
 
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication
@@ -9,6 +10,7 @@ from rest_framework.permissions import IsAuthenticatedOrReadOnly ,IsAuthenticate
 from .permissions import IsAuthorOrReadOnly
 
 
+#전체확인용
 class MessageViewSet(ModelViewSet):
     # #authentication 추가 
     # authentication_classes = [BasicAuthentication, SessionAuthentication]
@@ -27,8 +29,9 @@ class MessageViewSet(ModelViewSet):
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        # 유효성 검사가 성공한 경우에만 편지를 생성
+        
         self.perform_create(serializer)
+        
         #칭찬 보내는 사람 포인트부여
         serializer.instance.writer.userinfo.points += 10
         serializer.instance.writer.userinfo.save()
@@ -36,17 +39,49 @@ class MessageViewSet(ModelViewSet):
         receiver = serializer.validated_data.get('receiver') #받은 사람 정보 가져오기
         receiver.userinfo.count += 1
         receiver.userinfo.save()
-        # 편지생성
-        self.perform_create(serializer)
+        
+
         headers = self.get_success_headers(serializer.data) #성공시 헤더로 전달할 값들을 headers에 담기
         return Response(serializer.data,headers=headers) #response로 data, headers 전송 
 
-
+#개개인 메세지 전달
 class ReceivedMessageViewSet(ModelViewSet):
     
     serializer_class = MessageSerializer
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
-        queryset = Message.objects.filter(receiver=self.request.user)
+        user_id = self.kwargs['user_id']
+        queryset = Message.objects.filter(receiver_id=user_id)
         return queryset
+    
+    #유저 지정
+    def perform_create(self, serializer) :
+        serializer.save(writer=self.request.user) 
+
+    def create(self, request, *args, **kwargs):
+        user_id = self.kwargs['user_id'] #받는사람
+        receiver = get_object_or_404(User, pk=user_id)
+        
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        
+        
+        serializer.validated_data['writer'] = request.user #작성자 설정
+        serializer.validated_data['receiver'] = receiver
+
+        # 편지를 생성
+        self.perform_create(serializer)
+
+        #칭찬 보내는 사람 포인트부여
+        serializer.instance.writer.userinfo.points += 10
+        serializer.instance.writer.userinfo.save()
+
+        #칭찬 받는 사람 카운트 증가 
+        receiver = serializer.validated_data.get('receiver') #받은 사람 정보 가져오기
+        receiver.userinfo.count += 1
+        receiver.userinfo.save()
+        
+        headers = self.get_success_headers(serializer.data) #성공시 헤더로 전달할 값들을 headers에 담기
+        return Response(serializer.data,headers=headers) #response로 data, headers 전송 
+
